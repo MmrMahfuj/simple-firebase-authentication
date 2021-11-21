f4cd9a9586430af058094bb345d4c2c852915727
MmrMahfuj <mahfuj.mmr3@gmail.com>
2021-11-21 20:09:46 +0600

    simple firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCG-2LjoyA5wVGvB6JJKuqcd-y4plwf5o",
+    authDomain: "simple-firebase-authenti-c8e38.firebaseapp.com",
+    projectId: "simple-firebase-authenti-c8e38",
+    storageBucket: "simple-firebase-authenti-c8e38.appspot.com",
+    messagingSenderId: "873802425137",
+    appId: "1:873802425137:web:ee52cac24653454e664610"
+};
+
+export default firebaseConfig;
